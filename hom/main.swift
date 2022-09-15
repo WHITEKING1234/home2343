e924c61780957d1727442eb0299bcd39a3a4e6a3
bombag456 <bombag456@gmail.com>
2022-09-15 15:39:42 +0600

    dadascasc

--- hom/main.swift
+++ hom/main.swift
@@ -9,3 +9,44 @@ import Foundation
 
 print("Hello, World!")
 
+func add(num1:Int){
+    print("будет-\(num1+100)")
+}
+let add=readLine()!
+print("Ща дай подумать-новерное будет")
+add(num1: 100)
+
+func add2(num2:Double){
+    print("Будет \(num2-3.14)")
+}
+
+let add2=readLine()!
+add2(num2: 2.03)
+
+
+func add3(num3:Double){
+    print("Pi=\(3.14)")
+}
+let add3=readLine()!
+add3(num3: 3.14)
+
+func add4(num4:Int){
+    print("Думаю будет\(num4*5)")
+}
+let add4=readLine()!
+add4(num4: 5)
+print("Знаешь не стоить верить всему что я говрорю я же могу и ошибатся:)")
+func add5(num5:String){
+    print("Ок вижу что ты учишься в Geaktehc идем дальше\(num5)")
+}
+print("Ты мне веришь?")
+let num5 = readLine()!
+add5(num5:num5)
+
+
+func add6(num6:Int){
+    print("Будет\(num6+67)")
+}
+let add6=readLine()!
+add6(num6: -20)
+          
